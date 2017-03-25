eab4319d2ca445e6c4d5df85f1448af81775c247
Cameron Sinko <csinko@gmail.com>
2017-03-25 17:58:14 -0400

    users

--- users.go
+++ users.go
@@ -1,1 +1,26 @@
 package main
+
+//User struct connects Telegram and Capital One User
+type User struct {
+	TelegramID       string
+	TelegramUsername string
+	CapitalOneID     string
+}
+
+//Users is an array of the users
+type Users []User
+
+func InitUsers() {
+	csinko := User{
+		TelegramID:       "258303594",
+		TelegramUsername: "csinko",
+		CapitalOneID:     "",
+	}
+
+	kdog5 := User{
+		TelegramID:       "316190324",
+		TelegramUsername: "kdog5",
+		CapitalOneID:     "",
+	}
+
+}
